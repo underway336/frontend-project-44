b243a88b6576490171016c34a31c5bbeab221728
Andrew Sergienko <underway336@gmail.com>
2023-11-24 12:14:33 +0300

    add src/cli.js,add functions getWelcome, userName, grtGreeting

---
+++ src/cli.js
@@ -0,0 +1,13 @@
+import readlineSync from 'readline-sync';
+
+export const getWelcome = console.log("Welcome to the Brain Games!");
+
+export const userName = readlineSync.question('May I have your name? ');
+
+export const getGreeting = (userName) => {
+  console.log('Hello, ' + userName + '!');
+};
+
+
+
+
