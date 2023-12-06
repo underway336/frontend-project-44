f1484d09200ad6c3395a1135c3c7a5715f43bf25
Andrew Sergienko <underway336@gmail.com>
2023-12-07 00:00:34 +0300

    add game logic code calculator

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,3 +1,62 @@
-import { greeting } from '../index.js';
+import {
+    greeting,
+    rulesOfTheGame,
+    questionToUser,
+    getRandomNumber,
+    userAnswer,
+    answerIsCorrect,
+    congratulations,
+    wrongAnswer,
+    wrongAnswerTryAgain,
+} from '../index.js';
 
 greeting();
+
+rulesOfTheGame('What is the result of the expression?');
+
+for (let i = 0; i < 3; i += 1) {
+    const number1 = getRandomNumber(0, 25);
+    const number2 = getRandomNumber(0, 25);
+
+    const symbols = ['+', '-', '*'];
+    const operator = symbols.at(getRandomNumber(0, 3));
+
+    questionToUser(`Question: ${number1} ${operator} ${number2}`);
+
+    let correctAnswer = 0;
+
+    switch (operator) {
+        case '+':
+            correctAnswer = number1 + number2;
+            break;
+        case '-':
+            correctAnswer = number1 - number2;
+            break;
+        case '*':
+            correctAnswer = number1 * number2;
+            break;
+        default:
+            correctAnswer = number1 * number2;
+            break;
+    }
+
+    const stringSolution = userAnswer();
+
+    let solution = Number(stringSolution);
+
+    if (Number.isNaN(solution) === true) {
+        solution = stringSolution;
+    }
+
+    if (solution === correctAnswer) {
+        answerIsCorrect();
+
+        if (i === 2) {
+            congratulations();
+        }
+    } else {
+        wrongAnswer(solution, correctAnswer);
+        wrongAnswerTryAgain();
+        break;
+    }
+}
