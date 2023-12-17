cc1f6dab523c0dae1c720f29eccb4e482aa2359b
Andrew Sergienko <underway336@gmail.com>
2023-12-18 01:30:06 +0300

    added "brain-prime" game logic

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,3 +1,37 @@
-import { greeting } from '../index.js';
+import {
+    greeting,
+    rulesOfTheGame,
+    questionToUser,
+    getRandomNumber,
+    isPrimeNumber,
+    userAnswer,
+    answerIsCorrect,
+    congratulations,
+    wrongAnswer,
+    wrongAnswerTryAgain,
+} from '../index.js';
 
 greeting();
+
+rulesOfTheGame('Answer "yes" if given number is prime. Otherwise answer "no".');
+
+for (let i = 0; i < 3; i += 1) {
+    const number = getRandomNumber(0, 71);
+
+    questionToUser(`Question: ${number}`);
+
+    const solution = userAnswer();
+    const correctAnswer = isPrimeNumber(number);
+
+    if (solution === correctAnswer) {
+        answerIsCorrect();
+
+        if (i === 2) {
+            congratulations();
+        }
+    } else {
+        wrongAnswer(solution, correctAnswer);
+        wrongAnswerTryAgain();
+        break;
+    }
+}
