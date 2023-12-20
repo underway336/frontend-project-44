1bf108fb7271f87fc50b00037185bd1b337af016
Andrew Sergienko <underway336@gmail.com>
2023-12-20 23:50:58 +0300

    refactoring src/games/brain-prime.js, added function askQuestionAndCheckAnswer()

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,33 +1,12 @@
 import {
   greeting,
   rulesOfTheGame,
-  questionToUser,
-  getRandomNumber,
+  askQuestionAndCheckAnswer,
   isPrimeNumber,
-  userAnswer,
-  numberOfRepetitions,
-  stepSize,
-  isCorrect,
 } from '../index.js';
 
 greeting();
 
 rulesOfTheGame('Answer "yes" if given number is prime. Otherwise answer "no".');
 
-let repetitionCounter = 0;
-
-const minValue = 2;
-const maxValue = 71;
-
-for (repetitionCounter; repetitionCounter < numberOfRepetitions; repetitionCounter += stepSize) {
-  const displayNumber = getRandomNumber(minValue, maxValue);
-
-  questionToUser(`Question: ${displayNumber}`);
-
-  const solution = userAnswer();
-  const correctAnswer = isPrimeNumber(displayNumber);
-
-  const result = isCorrect(solution, correctAnswer, repetitionCounter);
-
-  if (result === false) break;
-}
+askQuestionAndCheckAnswer(2, 71, isPrimeNumber, (displayNumber) => isPrimeNumber(displayNumber));
