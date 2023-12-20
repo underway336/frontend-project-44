0660f8987db28ec27dde900c96c60fdd481ccb53
Andrew Sergienko <underway336@gmail.com>
2023-12-20 23:42:07 +0300

    refactoring src/games/brain-even.js, added function askQuestionAndCheckAnswer()

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,34 +1,12 @@
 import {
   greeting,
-  rulesOfTheGame,
-  questionToUser,
-  userAnswer,
   isEven,
-  getRandomNumber,
-  numberOfRepetitions,
-  stepSize,
-  isCorrect,
-
+  rulesOfTheGame,
+  askQuestionAndCheckAnswer,
 } from '../index.js';
 
 greeting();
 
 rulesOfTheGame('Answer "yes" if the number is even, otherwise answer "no".');
 
-let repetitionCounter = 0;
-
-const minValue = 1;
-const maxValue = 100;
-
-for (repetitionCounter; repetitionCounter < numberOfRepetitions; repetitionCounter += stepSize) {
-  const number = getRandomNumber(minValue, maxValue);
-
-  questionToUser(`Question: ${number}`);
-
-  const solution = userAnswer();
-  const correctAnswer = isEven(number);
-
-  const result = isCorrect(solution, correctAnswer, repetitionCounter);
-
-  if (result === false) break;
-}
+askQuestionAndCheckAnswer(2, 100, isEven, (displayNumber) => isEven(displayNumber));
