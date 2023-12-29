d8bd7807f4ee971fa15b2ab50ac91b53c43de008
Andrew Sergienko <underway336@gmail.com>
2023-12-30 01:30:47 +0300

    fix runBrainEven

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,12 +1,33 @@
-import {
-  greeting,
-  isEven,
-  rulesOfTheGame,
-  askQuestionAndCheckAnswer,
-} from '../index.js';
+import { cons } from '@hexlet/pairs';
+import { basicGeneralFunctionality } from '../index.js';
+import getRandomNumber from '../utils.js';
 
-greeting();
+const runBrainEven = () => {
+  const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-rulesOfTheGame('Answer "yes" if the number is even, otherwise answer "no".');
+  const minValue = 2;
+  const maxValue = 100;
 
-askQuestionAndCheckAnswer(2, 100, isEven, (displayNumber) => isEven(displayNumber));
+  const getRoundArr = [];
+
+  let countOfRounds = 0;
+  while (countOfRounds !== 3) {
+    const displayNumber = getRandomNumber(minValue, maxValue);
+    const questionToUser = (`Question: ${displayNumber}`);
+
+    const isEven = () => {
+      const result = displayNumber % 2 === 0 ? 'yes' : 'no';
+      return result;
+    };
+
+    const correctAnswer = isEven();
+    const getRound = cons(questionToUser, correctAnswer);
+
+    getRoundArr.push(getRound);
+
+    countOfRounds += 1;
+  }
+  return basicGeneralFunctionality(rules, getRoundArr);
+};
+
+export default runBrainEven;
