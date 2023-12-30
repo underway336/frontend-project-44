841aed146c0dcfd8381f9b9c735c8c1978e26c07
Andrew Sergienko <underway336@gmail.com>
2023-12-30 19:27:50 +0300

    update src/index.js

--- src/index.js
+++ src/index.js
@@ -33,17 +33,3 @@ export const basicGeneralFunctionality = (rules, getRoundArr) => {
 };
 
 export default basicGeneralFunctionality;
-
-// export const isPrimeNumber = (displayNumber) => {
-//   const minimumPrimeNumber = 2;
-//   if (displayNumber < minimumPrimeNumber) {
-//     return 'no';
-//   }
-//   const squareRoot = Math.round(Math.sqrt(displayNumber));
-//   for (let divider = minimumPrimeNumber; divider <= squareRoot; divider += 1) {
-//     if (displayNumber % divider === 0) {
-//       return 'no';
-//     }
-//   }
-//   return 'yes';
-// };
