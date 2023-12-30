245f694a3105874a8c315d48fc3a4532c773ea12
Andrew Sergienko <underway336@gmail.com>
2023-12-30 17:47:11 +0300

    added checkNaN

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,12 @@
+export const checkNaN = (userAnswer) => {
+  let solution = Number(userAnswer);
+
+  if (Number.isNaN(solution) === true) {
+    solution = userAnswer;
+  }
+  return solution;
+};
+
 const getRandomNumber = (minValue, maxValue) => {
   const randomNumber = Math.floor(Math.random() * (maxValue - minValue)) + minValue;
   return randomNumber !== 0 ? randomNumber : minValue;
