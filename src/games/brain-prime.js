1672d0c0dd685e4d38716e1c5dba21c82586fd8e
Andrew Sergienko <underway336@gmail.com>
2023-12-30 19:29:17 +0300

    fix brain-prime game logic

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,12 +1,42 @@
-import {
-  greeting,
-  rulesOfTheGame,
-  askQuestionAndCheckAnswer,
-  isPrimeNumber,
-} from '../index.js';
+import { cons } from '@hexlet/pairs';
+import { basicGeneralFunctionality } from '../index.js';
+import getRandomNumber from '../utils.js';
 
-greeting();
+const runBrainPrime = () => {
+  const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-rulesOfTheGame('Answer "yes" if given number is prime. Otherwise answer "no".');
+  const minValue = 2;
+  const maxValue = 100;
 
-askQuestionAndCheckAnswer(2, 71, isPrimeNumber, (displayNumber) => isPrimeNumber(displayNumber));
+  const getRoundArr = [];
+
+  let countOfRounds = 0;
+  while (countOfRounds !== 3) {
+    const displayNumber = getRandomNumber(minValue, maxValue);
+    const questionToUser = (`Question: ${displayNumber}`);
+
+    const isPrimeNumber = () => {
+      const minimumPrimeNumber = 2;
+      if (displayNumber < minimumPrimeNumber) {
+        return 'no';
+      }
+      const squareRoot = Math.round(Math.sqrt(displayNumber));
+      for (let divider = minimumPrimeNumber; divider <= squareRoot; divider += 1) {
+        if (displayNumber % divider === 0) {
+          return 'no';
+        }
+      }
+      return 'yes';
+    };
+
+    const correctAnswer = isPrimeNumber();
+
+    const getRound = cons(questionToUser, correctAnswer);
+    getRoundArr.push(getRound);
+
+    countOfRounds += 1;
+  }
+  return basicGeneralFunctionality(rules, getRoundArr);
+};
+
+export default runBrainPrime;
