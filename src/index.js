785f48f1a99264cd221917e202e201516eb0cb86
Andrew Sergienko <underway336@gmail.com>
2024-01-09 14:58:28 +0300

    fix basicGeneralFunctionality

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import { car, cdr } from '@hexlet/pairs';
 import readlineSync from 'readline-sync';
 import { checkNaN } from './utils.js';
 
-export const basicGeneralFunctionality = (rules, getRoundArr) => {
+export const basicGeneralFunctionality = (rules, roundsArray) => {
   console.log('Welcome to the Brain Games!');
 
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-
   console.log(rules);
 
+  const roundCount = 3;
   let isCorrect = true;
 
-  for (let countOfRounds = 0; countOfRounds < 3 && isCorrect; countOfRounds += 1) {
-    console.log(car(getRoundArr[countOfRounds]));
+  for (let i = 0; i < roundCount && isCorrect; i += 1) {
+    console.log(car(roundsArray[i]));
 
     const userAnswer = checkNaN(readlineSync.question('Your answer: '));
 
-    const correctAnswer = cdr(getRoundArr[countOfRounds]);
+    const correctAnswer = cdr(roundsArray[i]);
 
     if (userAnswer !== correctAnswer) {
       console.log(
