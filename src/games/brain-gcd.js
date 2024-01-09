88e9776eccd0d9207d285c0daee87c508512a950
Andrew Sergienko <underway336@gmail.com>
2024-01-09 16:57:09 +0300

    fix runBrainGcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -2,44 +2,32 @@ import { cons } from '@hexlet/pairs';
 import { basicGeneralFunctionality } from '../index.js';
 import getRandomNumber from '../utils.js';
 
+const getGcd = (number1, number2) => {
+  let res = 0;
+  let max = Math.max(number1, number2);
+  let min = Math.min(number1, number2);
+  let remainder;
+
+  while (remainder !== 0) {
+    remainder = max % min;
+    max = min;
+    min = remainder;
+    res = max;
+  }
+  return res;
+};
+
 const runBrainGcd = () => {
   const rules = 'Find the greatest common divisor of given numbers.';
+  const roundCount = 3;
+  const getRound = [];
 
-  const minValue = 1;
-  const maxValue = 100;
-
-  const getRoundArr = [];
-
-  let countOfRounds = 0;
-  while (countOfRounds !== 3) {
-    const number1 = getRandomNumber(minValue, maxValue);
-    const number2 = getRandomNumber(minValue, maxValue);
-
-    const questionToUser = (`Question: ${number1} ${number2}`);
-
-    const getGcd = () => {
-      let res = 0;
-      let max = Math.max(number1, number2);
-      let min = Math.min(number1, number2);
-      let remainder;
-
-      while (remainder !== 0) {
-        remainder = max % min;
-        max = min;
-        min = remainder;
-        res = max;
-      }
-      return res;
-    };
-
-    const correctAnswer = getGcd(number1, number2);
-
-    const getRound = cons(questionToUser, correctAnswer);
-    getRoundArr.push(getRound);
-
-    countOfRounds += 1;
+  for (let i = 0; i < roundCount; i += 1) {
+    const number1 = getRandomNumber(1, 100);
+    const number2 = getRandomNumber(1, 100);
+    getRound.push(cons(`Question: ${number1} ${number2}`, getGcd(number1, number2)));
   }
-  return basicGeneralFunctionality(rules, getRoundArr);
+  return basicGeneralFunctionality(rules, getRound);
 };
 
 export default runBrainGcd;
