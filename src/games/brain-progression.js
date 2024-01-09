15acae53ff862ccb0850906afb65e46bbc2fce56
Andrew Sergienko <underway336@gmail.com>
2024-01-09 14:31:37 +0300

    fix runBrainProgression

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,13 +2,28 @@ import { cons, car, cdr } from '@hexlet/pairs';
 import { basicGeneralFunctionality } from '../index.js';
 import getRandomNumber from '../utils.js';
 
-const getProgression = (startProgression, progLength, progressionStep) => {
+const getProgression = (start, length, step) => {
   const progression = [];
-  for (let i = 0; i < progLength; i += 1) {
-    const number = startProgression + progressionStep * i;
+  for (let i = 0; i < length; i += 1) {
+    const number = start + step * i;
     progression.push(number);
   }
   return progression;
+};
+
+const getMissingNumber = (progression, index) => cons(progression[index], '..');
+
+const replaceMissingNumber = (progression, missingNumber) => {
+  const resultProgression = [];
+  for (let i = 0; i < progression.length; i += 1) {
+    const number = progression[i];
+    if (number === car(missingNumber)) {
+      resultProgression.push(cdr(missingNumber));
+    } else {
+      resultProgression.push(number);
+    }
+  }
+  return resultProgression;
 };
 
 const runBrainProgression = () => {
@@ -17,22 +32,14 @@ const runBrainProgression = () => {
   const getRound = [];
 
   for (let i = 0; i < roundCount; i += 1) {
-    const startProgression = getRandomNumber(2, 50);
-    const progressionStep = getRandomNumber(2, 10);
-    const progLength = getRandomNumber(5, 10);
-    const progression = getProgression(startProgression, progLength, progressionStep);
+    const start = getRandomNumber(2, 50);
+    const step = getRandomNumber(2, 10);
+    const length = getRandomNumber(5, 10);
+    const progression = getProgression(start, length, step);
 
-    const randomIndex = getRandomNumber(0, (progLength - 1));
-    const missingNumber = cons(progression[randomIndex], '..');
-    const resultProgression = [];
-
-    for (let index = 0; index < progLength; index += 1) {
-      if (progression[index] === progression[randomIndex]) {
-        resultProgression.push(cdr(missingNumber));
-      } else {
-        resultProgression.push(progression[index]);
-      }
-    }
+    const randomIndex = getRandomNumber(0, (length - 1));
+    const missingNumber = getMissingNumber(progression, randomIndex);
+    const resultProgression = replaceMissingNumber(progression, missingNumber);
     getRound.push(cons(`Question: ${resultProgression.join(' ')}`, car(missingNumber)));
   }
   return basicGeneralFunctionality(rules, getRound);
