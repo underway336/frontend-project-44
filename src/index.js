5ea0f1f0e9631f5c69b342466dc6b826a368c7a1
Andrew Sergienko <underway336@gmail.com>
2024-01-09 15:19:34 +0300

    fix basicGeneralFunctionality

--- src/index.js
+++ src/index.js
@@ -4,32 +4,28 @@ import { checkNaN } from './utils.js';
 
 export const basicGeneralFunctionality = (rules, roundsArray) => {
   console.log('Welcome to the Brain Games!');
-
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(rules);
 
   const roundCount = 3;
-  let isCorrect = true;
 
-  for (let i = 0; i < roundCount && isCorrect; i += 1) {
+  for (let i = 0; i < roundCount; i += 1) {
     console.log(car(roundsArray[i]));
 
     const userAnswer = checkNaN(readlineSync.question('Your answer: '));
-
     const correctAnswer = cdr(roundsArray[i]);
 
-    if (userAnswer !== correctAnswer) {
-      console.log(
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      return console.log(
         `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.\nLet's try again, ${userName}!`,
       );
-      isCorrect = false;
-    } else {
-      console.log('Correct!');
     }
   }
 
-  if (isCorrect) {
+  if (roundCount) {
     console.log(`Congratulations, ${userName}!`);
   }
 };
