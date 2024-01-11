d87d9d1dedb472c773d1f9baa81071f5d960de9e
Andrew Sergienko <underway336@gmail.com>
2024-01-11 12:49:30 +0300

    update src/games/brain-prime.js, fix isPrimeNumber

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -1,5 +1,5 @@
 import { basicGeneralFunctionality } from '../index.js';
-import { getRandomNumber, getRound, roundCount } from '../utils.js';
+import { runBrainGame } from '../utils.js';
 
 const isPrimeNumber = (displayNumber) => {
   const minimumPrimeNumber = 2;
@@ -17,13 +17,7 @@ const isPrimeNumber = (displayNumber) => {
 
 const runBrainPrime = () => {
   const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
-  const roundsArray = [];
-
-  for (let i = 0; i < roundCount; i += 1) {
-    const displayNumber = getRandomNumber(2, 100);
-    roundsArray.push(getRound(`Question: ${displayNumber}`, isPrimeNumber(displayNumber)));
-  }
-  return basicGeneralFunctionality(rules, roundsArray);
+  return basicGeneralFunctionality(rules, runBrainGame(isPrimeNumber));
 };
 
 export default runBrainPrime;
