d0217009469ff3d727e2b2a52a239efbcf3c9570
Andrew Sergienko <underway336@gmail.com>
2024-01-25 01:17:35 +0300

    update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,20 +1,6 @@
-import { cons } from '@hexlet/pairs';
-
-export const getRandomNumber = (minValue, maxValue) => {
+const getRandomNumber = (minValue, maxValue) => {
   const randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
   return randomNumber;
 };
 
-export const getRound = (question, correctAnswer) => {
-  const round = cons(question, correctAnswer);
-  return round;
-};
-
-export const runBrainGame = (questionCallback, roundsCount) => {
-  const rounds = [];
-  for (let i = 0; i < roundsCount; i += 1) {
-    const displayNumber = getRandomNumber(2, 100);
-    rounds.push(getRound(`Question: ${displayNumber}`, questionCallback(displayNumber)));
-  }
-  return rounds;
-};
+export default getRandomNumber;
