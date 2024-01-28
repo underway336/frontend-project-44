1daf571a59c20a1c81c309198d2eaf882064afc5
Andrew Sergienko <underway336@gmail.com>
2024-01-28 13:24:01 +0300

    added function generateRounds & import roundsCount

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,16 @@
-const getRandomNumber = (minValue, maxValue) => {
+import { cons } from '@hexlet/pairs';
+import { roundsCount } from './index.js';
+
+export const getRandomNumber = (minValue, maxValue) => {
   const randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
   return randomNumber;
 };
 
-export default getRandomNumber;
+export const generateRounds = (getRound) => {
+  const rounds = [];
+  for (let i = 0; i < roundsCount; i += 1) {
+    const round = getRound();
+    rounds.push(cons(round[0], round[1]));
+  }
+  return rounds;
+};
