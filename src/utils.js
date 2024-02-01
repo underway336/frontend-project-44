d7c835709d60e4c7b3308ef61cd11bfa35f13690
Andrew Sergienko <underway336@gmail.com>
2024-02-01 11:45:41 +0300

    update src/utils.js: fixed getRandomNumber, del generateRounds

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,2 @@
-import { cons } from '@hexlet/pairs';
-import { roundsCount } from './index.js';
-
-export const getRandomNumber = (minValue, maxValue) => {
-  const randomNumber = Math.floor(Math.random() * (maxValue - minValue + 1)) + minValue;
-  return randomNumber;
-};
-
-export const generateRounds = (getRound) => {
-  const rounds = [];
-  for (let i = 0; i < roundsCount; i += 1) {
-    const round = getRound();
-    rounds.push(cons(round[0], round[1]));
-  }
-  return rounds;
-};
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+export default getRandomNumber;
