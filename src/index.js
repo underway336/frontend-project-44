6015085fe32d5b38c4a96678c2c7275652963318
Andrew Sergienko <underway336@gmail.com>
2024-02-01 11:48:32 +0300

    update src/index.js: optimized round generator loop

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
-import { car, cdr } from '@hexlet/pairs';
 import readlineSync from 'readline-sync';
 
 export const roundsCount = 3;
 
-export const runGame = (rules, rounds) => {
+export const runGame = (rules, getRound) => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
   console.log(rules);
   for (let i = 0; i < roundsCount; i += 1) {
-    console.log(car(rounds[i]));
+    const round = getRound();
+    console.log(round[0]);
 
     const userAnswer = readlineSync.question('Your answer: ');
-    const correctAnswer = cdr(rounds[i]).toString();
+    const correctAnswer = round[1];
 
     console.log('Correct!');
 
