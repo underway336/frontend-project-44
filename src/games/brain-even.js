04cedbdf32a682552a9c8e27f09d8ab1c0a2377e
Andrew Sergienko <underway336@gmail.com>
2024-02-01 11:55:41 +0300

    update src/games/brain-even.js

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,5 +1,5 @@
 import { runGame } from '../index.js';
-import { getRandomNumber, generateRounds } from '../utils.js';
+import getRandomNumber from '../utils.js';
 
 const isEven = (displayNumber) => {
   const correctAnswer = displayNumber % 2 === 0 ? 'yes' : 'no';
@@ -7,15 +7,15 @@ const isEven = (displayNumber) => {
 };
 
 const getRound = () => {
-  const displayNumber = getRandomNumber(2, 100);
+  const displayNumber = getRandomNumber(2, 100).toString();
 
   return [`Question: ${displayNumber}`, isEven(displayNumber)];
 };
 
 const runBrainEven = () => {
   const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
-  const rounds = generateRounds(getRound);
-  return runGame(rules, rounds);
+  const round = getRound;
+  return runGame(rules, round);
 };
 
 export default runBrainEven;
