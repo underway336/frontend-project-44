18747f5073a24af3ed269c030174100ddd0be0c3
Andrew Sergienko <underway336@gmail.com>
2024-02-04 20:04:35 +0300

    const question fixed

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -22,7 +22,7 @@ const getRound = () => {
   const number1 = getRandomNumber(1, 100);
   const number2 = getRandomNumber(1, 100);
 
-  const question = `${number1} ${number2}`.toString();
+  const question = `${number1} ${number2}`;
   const answer = getGcd(number1, number2).toString();
   return [question, answer];
 };
