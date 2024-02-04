8185e775208d0f6291df8da5fdfe8664219d96fb
Andrew Sergienko <underway336@gmail.com>
2024-02-04 21:56:16 +0300

    checkProperty del

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -1,5 +1,5 @@
 import { runGame } from '../index.js';
-import { getRandomNumber, checkProperty } from '../utils.js';
+import { getRandomNumber } from '../utils.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
@@ -9,7 +9,7 @@ const getRound = () => {
   const number = getRandomNumber(2, 100);
 
   const question = number.toString();
-  const answer = checkProperty(isEven, number);
+  const answer = isEven(number) ? 'yes' : 'no';
   return [question, answer];
 };
 
