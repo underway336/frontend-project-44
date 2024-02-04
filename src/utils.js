407909fa4a634b2f77f149223523d1973025d289
Andrew Sergienko <underway336@gmail.com>
2024-02-04 19:52:44 +0300

    getRandomIndex function fixed

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-export const getRandomIndex = (col) => getRandomNumber(0, col.length - 1);
+export const getRandomIndex = (collection) => getRandomNumber(0, collection.length - 1);
