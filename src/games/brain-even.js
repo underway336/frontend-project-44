77cf37964c1fe679bbbbf32398e0915f83229e69
Andrew Sergienko <underway336@gmail.com>
2024-02-04 19:49:02 +0300

    isEven function fixed

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -3,13 +3,13 @@ import { getRandomNumber } from '../utils.js';
 
 const rules = 'Answer "yes" if the number is even, otherwise answer "no".';
 
-const isEven = (displayNumber) => (displayNumber % 2 === 0 ? 'yes' : 'no');
+const isEven = (number) => number % 2 === 0;
 
 const getRound = () => {
-  const displayNumber = getRandomNumber(2, 100);
+  const number = getRandomNumber(2, 100);
 
-  const question = displayNumber.toString();
-  const answer = isEven(displayNumber).toString();
+  const question = number.toString();
+  const answer = isEven(number) ? 'yes' : 'no';
   return [question, answer];
 };
 
